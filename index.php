472523c6d5cc8892e5fbe215de509bdefcfec888
global_1981 <global_1981@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-05 23:43:07 +0000

    Merge branch 'master' of /Users/bencooling/Sites/wordpress/wordpress-3.3.2/wp-content/plugins/flickree
    git-svn-id: https://plugins.svn.wordpress.org/flickree/trunk@540360 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: flickree
 Plugin URI: http://bcooling.com.au
 Description: Dynamically pull in photos from flickr based on the photoset, group or gallery, a search or a single photo id.
-Version: 0.2
+Version: 0.3
 Author: Ben Cooling
 Author URI: http://bcooling.com.au
 License: Copyright Ben Cooling
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://bcooling.com.au
 Tags: flickr, api, images, gallery, photo, yahoo, flickree
 Requires at least: 3.3.1
 Tested up to: 3.3.1
-Stable tag: 0.2
+Stable tag: 0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
